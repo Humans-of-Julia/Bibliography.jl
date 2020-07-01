e3d77b21cda0e2b55871e239a3e30386995c36a0
azzaare <jf@baffier.fr>
2020-07-01 17:03:36 +0900

    Requirements for Registrator.jl and cleanup

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 import Bibliography
 using Test
 
-for file in ["test.bib" , "test2.bib"]
+for file in ["test.bib"]
     test_import = Bibliography.import_bibtex("../examples/$file")
     println(typeof(test_import))
     for e in test_import
@@ -9,5 +9,5 @@ for file in ["test.bib" , "test2.bib"]
     end    
     println("publications: $(Bibliography.export_web(test_import))")
     println(Bibliography.export_bibtex("result.bib", test_import))
-    # println("Bibliography of Baffier for Web export:\n$(Bibliography.bibtex_to_web("../examples/baffier.bib"))")
+    rm("result.bib")
 end
