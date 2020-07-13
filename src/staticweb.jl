bfc925bb3ce1a1b58e39ed27c983b414eeb534f9
azzaare <jf@baffier.fr>
2020-07-13 23:41:06 +0900

    Updated xnames for documenter plugin

--- src/staticweb.jl
+++ src/staticweb.jl
@@ -12,24 +12,136 @@ function xtitle(entry::T) where T <: BibInternal.AbstractEntry
     return :title ∈ fieldnames(typeof(entry)) ? entry.title : get(entry.fields, "title", "")
 end
 
-function xnames(entry::BibInternal.AbstractEntry, editors::Bool=false)
+struct BibtexName  
+    particle::String
+    last::String
+    junior::String
+    first::String
+    middle::String  
+end
+
+function string_to_bibtex_name(str::String)
+    subnames = split(str, r"[\n\r ]*,[\n\r ]*")
+    # for s in subnames
+    #     println("s = $s")
+    # end
+    
+    # subnames containers
+    first::String = ""
+    middle::String = ""
+    particle::String = ""
+    last::String = ""
+    junior::String = ""
+
+    # mark for string parsing
+    mark_in = 1
+    mark_out = 0
+
+    # BibTeX form 1: First Second von Last
+    if length(subnames) == 1
+        aux = split(subnames[1], r"[\n\r ]+")
+        mark_out = length(aux) - 1
+        last = aux[end]
+        if length(aux) > 1 && isuppercase(aux[1][1])
+            first = aux[1]
+            for s in aux[2:end-1]
+                mark_in += 1
+                if islowercase(s[1])
+                    break;
+                end
+                middle *= " $s"
+            end
+            for s in reverse(aux[mark_in:mark_out])
+                if islowercase(s[1])
+                    break;
+                end
+                mark_out -= 1
+                last = "$s " * last
+            end
+            for s in aux[mark_in:mark_out]
+                particle *= " $s"
+            end
+        end
+    end
+    # BibTeX form 2: von Last, First Second
+    if length(subnames) == 2
+        aux = split(subnames[1], r"[\n\r ]+") # von Last
+        mark_out = length(aux) - 1
+        last = string(aux[end])
+        for s in reverse(aux[1:mark_out])
+            if islowercase(s[1])
+                break;
+            end
+            mark_out -= 1
+            last = "$s " * last
+        end
+        for s in aux[1:mark_out]
+            particle *= " $s"
+        end
+        aux = split(subnames[2], r"[\n\r ]+")
+        # println("aux = $aux")
+        first = aux[1]
+        if length(aux) > 1
+            for s in aux[2:end]
+                middle *= " $s"
+            end
+        end
+    end
+    if length(subnames) == 3
+        aux = split(subnames[1], r"[\n\r ]+") # von Last
+        mark_out = length(aux) - 1
+        last = aux[end]
+        for s in reverse(aux[1:mark_out])
+            if islowercase(s[1])
+                break;
+            end
+            mark_out -= 1
+            last = "$s " * last
+        end
+        for s in aux[1:mark_out]
+            particle *= " $s"
+        end
+        junior = subnames[2]
+        aux =split(subnames[3], r"[\n\r ]+")
+        first = aux[1]
+        if length(aux) > 1
+            for s in aux[2:end]
+                middle *= " $s"
+            end
+        end
+    end
+
+    # for t in map(typeof, [particle, last, junior, first, middle])
+    #     println("type = $t")
+    # end
+    return BibtexName(particle, last, junior, first, middle)
+end
+
+function xnames(
+    entry::BibInternal.AbstractEntry,
+    editors::Bool=false;
+    names::Symbol=:full # Current options: :last, :full
+    )
+    # forces the names to be editors' name if the entry are Proceedings
     if !editors && typeof(entry) ∈ [Proceedings]
         return xnames(entry, true)
     end
     names = editors ? entry.editor : entry.author
     str = ""
+
+    split_names = split(names, r"[\n\r ]and[\n\r ]")
     start = true
-    stop = 1
-    for s in split(names, r"[\n\r ]and[\n\r ]")
-        aux = split(s, r"[\n\r ]*,[\n\r ]*")
+    for s in split_names
+        bib = string_to_bibtex_name(String(s))
         str *= start ? "" : ", "
         start = false
-        str *= aux[end]
-        for t in aux[1:end - 1]
-            str *= " " * t
+        if names == :last
+            str *= bib.particle * " " * bib.last * " " * bib.junior
+        else
+            str *= bib.first * " " * bib.middle * " " * bib.particle * " " * bib.last * " " * bib.junior
         end
     end
-    return str
+    return replace(str, r"[\n\r ]+" => " ") # TODO: hack for extra space char, make it cleaner
 end
 
 # handle all entries
