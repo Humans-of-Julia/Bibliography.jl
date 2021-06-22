9657f18b7fbd63c0cd1f3358117598f6c88473ce
Max Horn <max@quendi.de>
2021-06-22 15:57:11 +0200

    Fix typos and an outdated link

--- src/staticweb.jl
+++ src/staticweb.jl
@@ -218,7 +218,7 @@ end
 
 """
     export_web(bibliography::DataStructures.OrderedDict{String,BibInternal.Entry})
-Export a biblography in internal format to the web format of the [StaticWebPages.jl](https://github.com/Humans-of-Julia/StaticWebPages.jl) pakcage. Also used by [DocumenterCitations.jl](https://github.com/ali-ramadhan/DocumenterCitations.jl).
+Export a bibliography in internal format to the web format of the [StaticWebPages.jl](https://github.com/Humans-of-Julia/StaticWebPages.jl) package. Also used by [DocumenterCitations.jl](https://github.com/ali-ramadhan/DocumenterCitations.jl).
 """
 function export_web(bibliography)
     entries = Vector{Publication}()
